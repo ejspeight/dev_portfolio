647f95d4e826927ee82b203b64556199eb873e59
Edward <62318118+ejspeight@users.noreply.github.com>
2024-02-16 23:12:27 +0800

    GET all posts api

--- backend/main.py
+++ backend/main.py
@@ -1,40 +1,10 @@
-from fastapi import FastAPI, HTTPException, Depends, status
-from pydantic import BaseModel
-from typing import Annotated
+from fastapi import FastAPI
+from routers import post_router
 import models
-from database import engine, SessionLocal
-from sqlalchemy.orm import Session
-
+from database import engine
 
 app = FastAPI()
+
+app.include_router(post_router.router)
+
 models.Base.metadata.create_all(bind=engine)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
-class PostBase(BaseModel):
-    title: str
-    content: str
-    author_id: int
-
-db_dependency = Annotated[Session, Depends(get_db)]
-
-@app.post("/posts/", status_code=status.HTTP_201_CREATED)
-async def create_post(post: PostBase, db: db_dependency):
-    db_post = models.Post(**post.dict())
-    db.add(db_post)
-    db.commit()
-    return post # returns post as response
-    
-
-@app.get("/posts/{post_id}", status_code=status.HTTP_200_OK)
-async def read_post(post_id: int, db: db_dependency):
-    post = db.query(models.Post).filter(models.Post.post_id == post_id).first()
-    if post is None:
-        raise HTTPException(status_code=404, detail = 'Post not found')
-    return post
-
---
+++ backend/routers/post_router.py
@@ -0,0 +1,42 @@
+from fastapi import APIRouter, HTTPException, Depends, status, Depends
+from sqlalchemy.orm import Session
+from pydantic import BaseModel
+from typing import List, Annotated
+import models
+from database import SessionLocal
+
+router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    author_id: int
+
+db_dependency = Depends(get_db)
+
+@router.post("/posts/", status_code=status.HTTP_201_CREATED)
+async def create_post(post: PostBase, db: Session = db_dependency):
+    db_post = models.Post(**post.dict())
+    db.add(db_post)
+    db.commit()
+    db.refresh(db_post)
+    return db_post
+
+@router.get("/posts/{post_id}", status_code=status.HTTP_200_OK)
+async def read_post(post_id: int, db: Session = db_dependency):
+    post = db.query(models.Post).filter(models.Post.post_id == post_id).first()
+    if post is None:
+        raise HTTPException(status_code=404, detail='Post not found')
+    return post
+
+# @router.get("/posts", response_model=List[models.Post], status_code=status.HTTP_200_OK)
+# async def get_all_posts(db: Session = db_dependency):
+#     posts = db.query(models.Post).all()
+#     return posts
