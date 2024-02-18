4bcd0d5136460fa90b2d0b4c557771c1dcea220c
Edward <62318118+ejspeight@users.noreply.github.com>
2024-02-18 21:46:41 +0800

    implement env variable db connection string and gitignore for .pyc files

--- backend/database.py
+++ backend/database.py
@@ -1,11 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
-SQLALCHEMY_DATABASE_URL = ""
+SQLALCHEMY_DATABASE_URL = os.environ.get("AWS_SQL_CONNECTION_STRING")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
