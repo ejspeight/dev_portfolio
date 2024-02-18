0a65c39efd0dd91e8233fc5525d5854ffbe4ca3d
Edward <62318118+ejspeight@users.noreply.github.com>
2024-02-18 21:12:32 +0800

    improve api error handling

--- backend/routers/post_router.py
+++ backend/routers/post_router.py
@@ -3,9 +3,12 @@ from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from typing import List, Annotated
 import models
+import logging
 from database import SessionLocal
 
+
 router = APIRouter()
+logger = logging.getLogger(__name__)
 
 def get_db():
     db = SessionLocal()
@@ -23,23 +26,36 @@ db_dependency = Depends(get_db)
 
 @router.post("/posts/", status_code=status.HTTP_201_CREATED)
 async def create_post(post: PostBase, db: Session = db_dependency):
-    db_post = models.Post(**post.dict())
-    db.add(db_post)
-    db.commit()
-    db.refresh(db_post)
-    return db_post
+    try:
+        db_post = models.Post(**post.dict())
+        db.add(db_post)
+        db.commit()
+        db.refresh(db_post)
+        return db_post
+    except Exception as error:
+        logger.exception('An error occurred while creating the post')
+        raise HTTPException(status_code=500, details = 'Internal server error')
+    
 
 @router.get("/posts/{post_id}", status_code=status.HTTP_200_OK)
 async def read_post(post_id: int, db: Session = db_dependency):
-    post = db.query(models.Post).filter(models.Post.post_id == post_id).first()
-    if post is None:
-        raise HTTPException(status_code=404, detail='Post not found')
-    return post
+    try:
+        post = db.query(models.Post).filter(models.Post.post_id == post_id).first()
+        if post is None:
+            raise HTTPException(status_code=404, detail='Post not found')
+        return post
+    except Exception as error:
+        logger.exception('An error occurred while retrieving the post')
+        raise HTTPException(status_code=500, details = 'Internal server error')
 
 
 @router.get("/posts/", status_code=status.HTTP_200_OK)
 async def get_all_posts(db: Session = db_dependency):
-    posts = db.query(models.Post).all()
-    if posts is None:
-        raise HTTPException(status_code=404, detail='Posts not found')
-    return posts
+    try:
+        posts = db.query(models.Post).all()
+        if posts is None:
+            raise HTTPException(status_code=404, detail='Posts not found')
+        return posts
+    except Exception as error:
+        logger.exception('An error occurred while retrieving all posts')
+        raise HTTPException(status_code=500, details = 'Internal server error')
